f35b0d0529b1ce6c80a4c44115d16a17fac44a2c
Jiang <wen900516@gmail.com>
2023-02-05 22:39:38 +0800

    uva 10035 python

---
+++ 10035/main.py
@@ -0,0 +1,26 @@
+while True:
+    try:
+        a, b = map(int , input().split())
+        cnt = 0
+        if(a == 0 and b == 0):
+            break
+        c = 0
+        for i in range(10):
+            c = a%10 + b%10 + c
+            if c > 9:
+                c = 1
+            else:
+                c = 0
+            cnt += c
+            a //= 10
+            b //= 10
+
+        if cnt == 0:
+            print('No carry operation.')
+        elif cnt == 1:
+            print('%d carry operation.' %cnt)
+        else:
+            print('%d carry operations.' %cnt)
+
+    except EOFError:
+        break
